28554975ec295a7c7604c4726f56cba41c7dde95
Hitsuji <erwann.mason@gmail.com>
2022-04-13 15:30:53 +0200

    avancement script python

--- APIcall.py
+++ APIcall.py
@@ -1,4 +1,5 @@
 import mariadb
+from mariadb import Error as OMGCAMARCHEPAS
 
 import json
 import requests
@@ -10,7 +11,7 @@ URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-a-par
       "=localisation_detail&facet=localisation_stationnement&facet=surface&facet=geo_point_2d"
 
 class DatabaseHandler:
-    def __init__(self, needUpdate=False,show=False):
+    def __init__(self, needUpdate=False, show=False):
         file = "error"
         if needUpdate or not os.path.isfile('json_data.json'):
             self.update()
@@ -28,7 +29,7 @@ class DatabaseHandler:
                 database="IGI-3014-TD9t-GP5"
             )
 
-        except mariadb.Error as e:
+        except OMGCAMARCHEPAS as e:
             print(f"Error connecting to MariaDB Platform: {e}")
             sys.exit(1)
 
@@ -36,6 +37,7 @@ class DatabaseHandler:
         self.cnx = cnx
         self.cur = cnx.cursor()
         self.insertFacets()
+        self.insertRecords()
 
     def update(self) -> None:
         res = requests.get(URL)
@@ -44,26 +46,74 @@ class DatabaseHandler:
             print("mauvaise réponse : ", res.status_code)
             sys.exit(1)
             
-        print("pull successful")
-        jsondata = res.json()  # On prend le json de la réponse
+        print("Requête réalisée avec succès !")
+        jsondata = res.json()
 
         with open('json_data.json', 'w') as outfile:
             json.dump(jsondata, outfile)
 
-        print("Update Done")
+        print("Mise à jour finie !")
 
     def insertFacets(self) -> None:
         moe = self.file["facet_groups"][2]["facets"]
-        moa = self.file["facet_groups"][3]["facets"]   
-        tailleStop = len(moe)
+        moa = self.file["facet_groups"][3]["facets"]
+        synthese = self.file["facet_groups"][4]["facets"]
+        detail = self.file["facet_groups"][5]["facets"]
+        impact = self.file["facet_groups"][6]["facets"]   
         
-        for i in range(tailleStop):
-            name = moe[i]["name"].replace("'", "\\'")
-            self.cur.execute(f"INSERT INTO Entite VALUES(NULL, '{name}');")
-        for j in range(len(moa)):
-            name = moa[j]["name"].replace("'", "\\'")
-            self.cur.execute(f"INSERT INTO Entite VALUES(NULL, '{name}');")
+        for facetMOE in moe:
+            value = facetMOE["name"].replace("'", "\\'")
+            self.cur.execute(f"INSERT INTO Entite VALUES(NULL, '{value}');")
+        for facetMOA in moa:
+            value = facetMOA["name"].replace("'", "\\'")
+            self.cur.execute(f"INSERT INTO Entite VALUES(NULL, '{value}');")
+        for facetSyn in synthese:
+            value = facetSyn["name"].replace("'", "\\'")
+            self.cur.execute(f"INSERT INTO NatureChantier VALUES(NULL, '{value}');")
+        for facetDet in detail:
+            value = facetDet["name"].replace("'", "\\'")
+            self.cur.execute(f"INSERT INTO Encombrement VALUES(NULL, '{value}');")
+        for facetImp in impact:
+            value = facetImp["name"].replace("'", "\\'")
+            self.cur.execute(f"INSERT INTO ImpactStationnement VALUES(NULL, '{value}');")
         self.cnx.commit()
+
+    def insertRecords(self) -> None:
+        records = self.file["records"]
+        for recordID, record in enumerate(records):
+            latitude = record["geometry"]["coordinates"][0]
+            longitude = record["geometry"]["coordinates"][1]
+            self.cur.execute(f"INSERT INTO Localisation VALUES({recordID}, {longitude}, {latitude});")
+            self.cnx.commit()
+
+            fields = record["fields"]
+            num = fields["num_emprise"]
+            surface = fields["surface"]
+            debut, fin = fields["date_debut"], fields["date_fin"]
+            chantier = fields["chantier_synthese"].replace("'", "\\'")
+            moe = fields["chantier_categorie"].replace("'", "\\'")
+            moa = fields["moa_principal"].replace("'", "\\'")
+            detail = fields["localisation_detail"].replace("'", "\\'")
+            station = fields["localisation_stationnement"].replace("'", "\\'")
+        
+            self.cur.execute(f"SELECT idLocalisation FROM Localisation WHERE 'Longitude' = {longitude} AND Latitude = {latitude};")
+            idLoc = self.cur.fetchone()[0]
+            self.cur.execute(f"SELECT idNatureChantier FROM NatureChantier WHERE Nature = {chantier};")
+            idNC = self.cur.fetchone()[0]
+            self.cur.execute(f"SELECT idEntite FROM Entite WHERE NomEntite = {moe};")
+            idMOE = self.cur.fetchone()[0]
+            self.cur.execute(f"SELECT idEntite FROM Entite WHERE NomEntite = {moa};")
+            idMOA = self.cur.fetchone()[0]
+            self.cur.execute(f"SELECT idEncombrement FROM Encombrement WHERE TypeEncombrement = {detail};")
+            idEnc = self.cur.fetchone()[0]
+            self.cur.execute(f"SELECT idStationnementImpact FROM ImpactStationnement WHERE TypeEncombrement = {station};")
+            idIS = self.cur.fetchone()[0]
+
+            self.cur.execute(f"INSERT INTO Chantier VALUES('{num}', {surface}, {debut}, {fin}, {idLoc}, {idNC}, {idMOE});")
+            self.cur.execute(f"INSERT INTO MOA VALUES('{num}', {idMOA});")
+            self.cur.execute(f"INSERT INTO TypeEncombrement VALUES('{num}', {idEnc});")
+            self.cur.execute(f"INSERT INTO TypeStationnementImpacte VALUES('{num}', {idIS});")
+            self.cnx.commit()
 
 if __name__ == '__main__':
     DatabaseHandler()
